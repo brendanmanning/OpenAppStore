ba3bde11b678112f5f20ba7ee0275b6b76f7ede6
Brendan Manning <BrendanManning19380@gmail.com>
2016-04-09 11:02:11 -0400

    Update config.php

--- config.php
+++ config.php
@@ -10,9 +10,9 @@
 	define("URL", "http://www.example.com/"); 
     /* The title of your website */
     /* It is shown on the menu bar, homepage, and various places across your website */
-	define("TITLE", "jLinux AppStore");
+	define("TITLE", "OpenAppStore");
     /* Subtitle is shown below the title on the homepage */
-	define("SUBTITLE", "Download apps for jLinux");
+	define("SUBTITLE", "Download apps for PC");
     /* This feature is not currently active - You may ignore this */
 	define("PREFIX", "ignore ");
     /* Your administrator email, will be shown in the contact menu */
